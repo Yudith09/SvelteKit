7ddb9954909d99c0686d2fb12ce03b94cf99313d
Yudith <yrecio@sifizsoft.com>
2024-07-18 08:29:26 -0400

    Primer componente

---
+++ src/routes/views/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+import Card from "../../components/Card.svelte";
+</script>
+
+<Card/>
+
+<style lang="postcss">
+	:global(html) {
+	  background-color: theme(colors.gray.100);
+	}
+</style>
