03e19dcf49cc9c377257d3d4878732763c08eef5
Yudith <yrecio@sifizsoft.com>
2024-07-19 10:04:17 -0400

    Agregando imagenes a la vista

---
+++ src/components/Image.svelte
@@ -0,0 +1,99 @@
+<div class="w-96 h-96 flex-col justify-start items-center gap-10 inline-flex">
+    <div class="w-96 h-36 p-5 bg-white rounded-bl-3xl rounded-br-3xl justify-between items-end inline-flex">
+      <div class="w-16 h-16 relative"></div>
+      <div class="h-20 justify-end items-center flex">
+        <div class="text-center text-sky-950 text-xl font-semibold font-['K2D'] leading-loose tracking-wide">1.000.000.000</div>
+        <div class="justify-start items-center gap-2.5 flex">
+          <img class="w-8 h-8" src="https://via.placeholder.com/32x32" />
+          <div class="w-8 h-8 relative">
+            <img class="w-8 h-8 left-0 top-0 absolute" src="https://via.placeholder.com/32x32" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="px-5 pt-1 pb-5 rounded-3xl flex-col justify-start items-center flex">
+      <div class="w-96 h-72 relative">
+        <div class="w-44 h-px left-0 top-[207px] absolute origin-top-left -rotate-90 border-4 border-sky-950"></div>
+        <div class="w-80 h-72 left-[10px] top-0 absolute">
+          <div class="w-80 h-60 left-0 top-0 absolute text-sky-950 text-5xl font-semibold font-['Mada'] leading-10">Connecting Businesses, Innovating Communications</div>
+          <div class="w-80 left-0 top-[237px] absolute text-stone-950/opacity-60 text-base font-normal font-['Khula'] leading-tight">Provides a brief overview of the company's specialization in providing VoIP services to small and medium-sized businesses.</div>
+        </div>
+      </div>
+      <img class="w-80 h-64 origin-top-left rotate-180" src="https://via.placeholder.com/350x261" />
+    </div>
+    <div class="self-stretch h-96 flex-col justify-start items-center gap-2.5 flex">
+      <div class="self-stretch h-96 px-32 py-5 bg-white rounded-3xl flex-col justify-center items-center gap-14 flex">
+        <div class="w-80 h-24 relative">
+          <div class="w-80 h-24 left-0 top-0 absolute text-center text-sky-950 text-4xl font-semibold font-['Mada'] leading-10">Everything you can do with Towitlinks</div>
+        </div>
+        <div class="w-80 h-52 relative">
+          <img class="w-12 h-12 left-[150px] top-0 absolute" src="https://via.placeholder.com/50x50" />
+          <div class="w-80 h-32 left-0 top-[80px] absolute flex-col justify-start items-center inline-flex">
+            <div class="self-stretch text-center text-sky-950 text-2xl font-semibold font-['Khula'] leading-9">VoIP Calls</div>
+            <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-loose">Make and receive calls from anywhere, anytime, with crystal-clear quality and cost-effective pricing.</div>
+          </div>
+        </div>
+      </div>
+      <div class="self-stretch h-72 px-32 py-5 bg-white rounded-3xl flex-col justify-center items-center gap-7 flex">
+        <img class="w-14 h-14" src="https://via.placeholder.com/60x60" />
+        <div class="h-40 flex-col justify-start items-center flex">
+          <div class="self-stretch text-center text-sky-950 text-2xl font-semibold font-['Khula'] leading-9">Text & Fax</div>
+          <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-loose">Stay connected with colleagues, partners, and clients through integrated text and fax services, eliminating the need for separate devices.</div>
+        </div>
+      </div>
+      <div class="self-stretch h-64 px-32 py-5 bg-white rounded-3xl flex-col justify-center items-center gap-7 flex">
+        <img class="w-12 h-12" src="https://via.placeholder.com/50x50" />
+        <div class="h-32 flex-col justify-start items-center flex">
+          <div class="self-stretch text-center text-sky-950 text-2xl font-semibold font-['Khula'] leading-9">Task Manager</div>
+          <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-loose">Organize your workload, track progress, and collaborate effectively with our intuitive task manager.</div>
+        </div>
+      </div>
+      <div class="self-stretch h-72 px-32 py-5 bg-white rounded-3xl flex-col justify-center items-center gap-7 flex">
+        <img class="w-14 h-14" src="https://via.placeholder.com/60x60" />
+        <div class="h-40 flex-col justify-start items-center flex">
+          <div class="self-stretch text-center text-sky-950 text-2xl font-semibold font-['Khula'] leading-9">Video Call</div>
+          <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-loose">Host clear, engaging video conferences with screen sharing, fostering stronger relationships and enhancing communication.</div>
+        </div>
+      </div>
+    </div>
+    <div class="h-96 p-5 bg-white rounded-3xl shadow flex-col justify-center items-center gap-6 flex">
+      <div class="text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-loose">GET YOUR FREE QUOTE NOW</div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-start items-center gap-2.5 inline-flex">
+        <div class="w-24 h-6 text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-loose">First Name</div>
+      </div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-start items-center gap-2.5 inline-flex">
+        <div class="w-24 h-6 text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-loose">Last Name</div>
+      </div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-between items-center inline-flex">
+        <div class="w-16 h-6 relative">
+          <div class="w-8 h-4 left-0 top-[8px] absolute text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-loose">+1</div>
+          <img class="w-8 h-6 left-[36px] top-0 absolute" src="https://via.placeholder.com/31x24" />
+        </div>
+        <div class="w-36 h-6 text-center text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-loose">Phone Number</div>
+      </div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-between items-center inline-flex">
+        <div class="w-32 h-12"><span style="text-sky-950/opacity-60 text-sm font-normal font-['Khula'] leading-tight">Country/Region</span><span style="text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-tight">      </span><span style="text-sky-950 text-base font-normal font-['Khula'] leading-tight">Select</span></div>
+        <img class="w-8 h-6" src="https://via.placeholder.com/31x24" />
+      </div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-between items-center inline-flex">
+        <div class="w-24 h-12"><span style="text-sky-950/opacity-60 text-sm font-normal font-['Khula'] leading-snug">Industry</span><span style="text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-tight">      </span><span style="text-sky-950 text-base font-normal font-['Khula'] leading-tight">Select</span></div>
+        <img class="w-8 h-6" src="https://via.placeholder.com/31x24" />
+      </div>
+      <div class="self-stretch h-12 px-5 py-2.5 bg-white rounded-2xl border border-sky-950 justify-between items-center inline-flex">
+        <div class="w-24 h-12"><span style="text-sky-950/opacity-60 text-sm font-normal font-['Khula'] leading-tight">Employers</span><span style="text-sky-950/opacity-60 text-base font-normal font-['Khula'] leading-tight">      </span><span style="text-sky-950 text-base font-normal font-['Khula'] leading-tight">Select</span></div>
+        <img class="w-8 h-6" src="https://via.placeholder.com/31x24" />
+      </div>
+      <div class="h-36 flex-col justify-start items-center gap-2.5 flex">
+        <div class="self-stretch h-20 p-4 bg-neutral-50 rounded justify-start items-start gap-2 inline-flex">
+          <div class="w-6 h-6 relative"></div>
+          <div class="grow shrink basis-0 text-sky-950 text-base font-normal font-['Khula'] leading-tight">This is a form element that is very long and will become taller the more text that is in it. </div>
+        </div>
+        <div class="w-56 h-14 px-14 py-3.5 bg-orange-400 rounded-2xl shadow justify-center items-center gap-2.5 inline-flex">
+          <div class="w-20 h-6 text-white text-base font-normal font-['Khula'] leading-loose tracking-wide">Get started</div>
+        </div>
+      </div>
+    </div>
+    <div class="self-stretch h-28 px-6 py-2.5 bg-cyan-400 flex-col justify-center items-center gap-5 flex">
+      <div class="text-center text-cyan-50 text-2xl font-semibold font-['Khula'] leading-9 tracking-widest">Trusted By 200k+    Business</div>
+    </div>
+  </div>
--- src/routes/views/+page.svelte
+++ src/routes/views/+page.svelte
@@ -1,11 +1,110 @@
 <script>
-import Card from "../../components/Card.svelte";
+	import newlogo from '$lib/imagesprop/newlogo.png'
+	import icons8 from '$lib/imagesprop/icons8.png'
+	import icons8_menu from '$lib/imagesprop/icons8_menu.png'
+	import smartmockups from '$lib/imagesprop/smartmockups.png'
+	import icons8_phone from '$lib/imagesprop/icons8_phone.png'
+    import icons8_phone_2 from '$lib/imagesprop/icons8_phone_2.png'
+	import icons8_phone_256 from '$lib/imagesprop/icons8_phone_256.png' 
+	import icons8_call from '$lib/imagesprop/icons8_call.png' 	
 </script>
 
-<Card/>
+<div class="all">
+<div class="component">
+  <div class="w-[390px] h-[152px] p-5 bg-white rounded-bl-3xl rounded-br-3xl justify-between items-end inline-flex">
+	<img alt="The project logo" src={newlogo} />
+	<div class="w-[70px] h-[70px] relative"></div>
+		<div class="h-[73px] justify-end items-center flex">
+	  <div class="text-center text-sky-950 text-xl font-semibold font-['K2D'] leading-[31px] tracking-wide">1.000.000.000</div>
+	  <div class="justify-start items-center gap-2.5 flex">		
+		<img class="w-8 h-8" src={icons8}/>
+		<div class="w-8 h-8 relative">
+		  <img class="w-8 h-8 left-0 top-0 absolute" src={icons8_menu}/>
+		</div>
+	  </div>
+	</div>
+  </div>
+</div>
+  <div class="componentExternal">
+  <div class="w-[360px] h-[297px] relative">
+	<div class="w-[180px] h-[0px] left-0 top-[207px] absolute origin-top-left -rotate-90 border-4 border-sky-950"></div>
+	<div class="w-[350px] h-[297px] left-[10px] top-0 absolute">
+	  <div class="w-[350px] h-[235px] left-0 top-0 absolute text-sky-950 text-[45px] font-semibold font-['Mada'] leading-[52px]">Connecting Businesses, Innovating Communications</div>
+	  <div class="w-[350px] left-0 top-[237px] absolute text-stone-950/opacity-60 text-base font-normal font-['Khula'] leading-tight">Provides a brief overview of the company's specialization in providing VoIP services to small and medium-sized businesses.</div>
+	</div>
+  </div>
+  <img class="w-[360px] h-[297px] relative" src={smartmockups} />  
+</div>
+
+<div class="component">
+  <div class="w-[390px] h-[412px] px-[125px] py-5 bg-white rounded-3xl flex-col justify-center items-center gap-[60px] inline-flex">
+	<div class="w-[348px] h-[101px] relative">
+	  <div class="w-[348px] h-[100px] left-0 top-0 absolute text-center text-sky-950 text-4xl font-semibold font-['Mada'] leading-10">Everything you can do with Towitlinks</div>
+	</div>
+	<div class="w-[350px] h-[211px] relative">
+	  <img class="w-[50px] h-[50px] left-[150px] top-0 absolute" src={icons8_phone} />
+	  <div class="w-[350px] h-[131px] left-0 top-[80px] absolute flex-col justify-start items-center inline-flex">
+		<div class="self-stretch text-center text-sky-950 text-[26px] font-semibold font-['Khula'] leading-[38px]">VoIP Calls</div>
+		<div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-[31px]">Make and receive calls from anywhere, anytime, with crystal-clear quality and cost-effective pricing.</div>
+	  </div>
+	</div>
+  </div>
+</div>
+<div class="component">
+  <div class="w-[390px] h-[292px] px-[125px] py-5 bg-white rounded-3xl flex-col justify-center items-center gap-[30px] inline-flex">
+	<img class="w-[60px] h-[60px]" src={icons8_phone_2} />
+	<div class="h-[162px] flex-col justify-start items-center flex">
+	  <div class="self-stretch text-center text-sky-950 text-[26px] font-semibold font-['Khula'] leading-[38px]">Text & Fax</div>
+	  <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-[31px]">Stay connected with colleagues, partners, and clients through integrated text and fax services, eliminating the need for separate devices.</div>
+	</div>
+  </div>
+</div>
+
+<div class="component">
+  <div class="w-[390px] h-[251px] px-[125px] py-5 bg-white rounded-3xl flex-col justify-center items-center gap-[30px] inline-flex">
+	<img class="w-[50px] h-[50px]" src={icons8_phone_256} />
+	<div class="h-[131px] flex-col justify-start items-center flex">
+	  <div class="self-stretch text-center text-sky-950 text-[26px] font-semibold font-['Khula'] leading-[38px]">Task Manager</div>
+	  <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-[31px]">Organize your workload, track progress, and collaborate effectively with our intuitive task manager.</div>
+	</div>
+  </div>   
+</div>
+
+<div class="component">
+  <div class="w-[390px] h-[292px] px-[125px] py-5 bg-white rounded-3xl flex-col justify-center items-center gap-[30px] inline-flex">
+	<img class="w-[60px] h-[60px]" src={icons8_call} />
+	<div class="h-[162px] flex-col justify-start items-center flex">		
+	  <div class= "self-stretch text-center text-sky-950 text-[26px] font-semibold font-['Khula'] leading-[38px] ">Video Call</div>
+	  <div class="self-stretch text-center text-sky-950/opacity-60 text-xl font-normal font-['Khula'] leading-[31px]">Host clear, engaging video conferences with screen sharing, fostering stronger relationships and enhancing communication.</div>
+	</div>
+   </div> 
+  </div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
 	  background-color: theme(colors.gray.100);
 	}
+
+	.all{
+		width:  390px;
+		height: 2,980px;
+		gap: 40px;		
+	}
+	.componentExternal{
+		width: Hug (390px)px;
+        height: Hug (578px)px;
+        padding: 5px 20px 20px 125px;
+        gap: 0px;
+        border-radius: 24px 0px 0px 0px;
+        opacity: 0px;
+	}
+	.component{
+		width: Fill (390px)px;
+		height: Hug (412px)px;
+		padding: 20px 125px 20px 125px;
+		gap: 60px;
+		border-radius: 24px 0px 0px 0px;
+		opacity: 0px;
+	}	
 </style>
